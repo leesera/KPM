bc8737d0979a88325c6f7bee0325da042d53c223
seralee <qseralee@gmail.com>
2018-01-17 19:18:22 +0900

    Add Downloader file

---
+++ Downloader.py
@@ -0,0 +1,22 @@
+import json
+from pprint import pprint
+import os
+
+download_path = "downloads/"
+
+json_data=open("output.json").read()
+
+datas = json.loads(json_data)
+
+for data in datas:
+  print data["version"]
+  path = os.path.join(download_path,data["target"])
+  path = os.path.join(path,data["version"])
+  cmd = "mkdir -p %s" % path
+  print(cmd)
+  os.system(cmd)
+  for filename in data["links"].keys():
+    link = data["links"][filename]
+    cmd = "wget %s -O %s" % (link, os.path.join(path,filename))
+    print(cmd)
+    os.system(cmd)
